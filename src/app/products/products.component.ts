bfc6531339f37ee3abe62bb7f7fbc8164842b0a8
Victor Barrios Jimenez <vhbj149601@gmail.com>
2017-02-01 18:15:40 -0600

    creating templates

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { ListComponent } from './list.component';
 
 @Component({
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+    
   }
 }
