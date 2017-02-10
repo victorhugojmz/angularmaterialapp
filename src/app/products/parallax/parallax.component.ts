81d868b27259e990fe8a9b454ca3eab1fc984d4b
Victor Barrios Jimenez <vhbj149601@gmail.com>
2017-02-09 21:21:25 -0600

    creating the componenr

--- src/app/products/parallax/parallax.component.ts
+++ src/app/products/parallax/parallax.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit , Input} from '@angular/core';
+import { Departamento } from '../../products';
 @Component({
   selector: 'app-parallax',
   templateUrl: './parallax.component.html',
   styleUrls: ['./parallax.component.css']
 })
 export class ParallaxComponent implements OnInit {
-
-  constructor() { }
-
+  departamento:  Departamento;
+  constructor() {
+    this.departamento.hero = "http://hoosierhomemade.com/wp-content/uploads/Walmart-Electronics-Project-Reboot.jpg"; 
+    this.departamento.nombre = "Electronics"; 
+  }
   ngOnInit() {
   }
 
