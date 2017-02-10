91cbfd966ad940b7cb0c96cb7ee09cca063f1853
Victor Barrios Jimenez <vhbj149601@gmail.com>
2017-02-09 23:26:17 -0600

    updating parallax component

--- src/app/products/parallax/parallax.component.ts
+++ src/app/products/parallax/parallax.component.ts
@@ -6,12 +6,8 @@ import { Departamento } from '../../products';
   styleUrls: ['./parallax.component.css']
 })
 export class ParallaxComponent implements OnInit {
-  hero;
-  nombre;
-  constructor() {
-    this.hero = "http://hoosierhomemade.com/wp-content/uploads/Walmart-Electronics-Project-Reboot.jpg"; 
-    this.nombre = "Electronics"; 
-  }
+  @Input() departamento: Departamento;
+  constructor() {}
   ngOnInit() {
   }
 
