2952b4d75e06d457b04dfedb5076ed2b77d294cf
Victor Barrios Jimenez <vhbj149601@gmail.com>
2017-02-11 19:22:43 -0600

    updating class

--- src/app/products/departamento.ts
+++ src/app/products/departamento.ts
@@ -3,17 +3,10 @@ export class Departamento {
     public descripcion ?: string;
     public hero: string;
     public imagenes: Array<string>;
-    public marca: Marca[];
-    constructor(_nombre: string, _descripcion: string, _hero: string,_imagenes: string[], _marca: Marca[]){
+    constructor(_nombre: string, _descripcion: string, _hero: string,_imagenes: string[]){
         this.nombre = _nombre;
         this.descripcion = _descripcion;
         this.hero = _hero;
         this.imagenes =  _imagenes;
-        this.marca = _marca;
     }  
-}
-export class Marca{
-    nombre: string;
-    descripcion: string;
-    total_articulos: string;
 }
--- src/app/products/products/products.component.ts
+++ src/app/products/products/products.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Producto } from '../../products';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-
   constructor() { }
-
   ngOnInit() {
   }
-
 }
