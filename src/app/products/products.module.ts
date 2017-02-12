daa3570d42d44b4d84f135ce840803047e676cf2
Victor Barrios Jimenez <vhbj149601@gmail.com>
2017-02-11 19:27:16 -0600

    updating module

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -3,20 +3,16 @@ import { CommonModule }   from '@angular/common';
 import { FormsModule }    from '@angular/forms';
 
 import { ProductsRoutingModule } from './products.routing.module';
-// Components
-import { ListItemComponent } from './list-item/list-item.component';
-import { DetailsComponent } from './details/details.component';
-import { DepartamentodetailsComponent } from './departamentodetails/departamentodetails.component';
-
 // Providers 
 import { ProductsService } from './products.service';
 import { LoaderComponent } from '../shared/loader/loader.component';
+// Components
+import { ListItemComponent } from './list-item/list-item.component';
+import { DetailsComponent } from './details/details.component';
 import { ContactComponent } from '../shared/contact/contact.component';
 import { AboutComponent } from '../shared/about.component';
 import { ParallaxComponent } from './parallax/parallax.component';
 import { ProductsComponent } from './products/products.component';
-
-
 @NgModule({
   imports: [
     CommonModule,
@@ -27,7 +23,6 @@ import { ProductsComponent } from './products/products.component';
     ProductsComponent,    
     ListItemComponent,
     DetailsComponent,
-    DepartamentodetailsComponent,
     LoaderComponent,
     ContactComponent,
     AboutComponent,
