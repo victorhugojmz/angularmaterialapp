b5f2782445830dcf9a70512d93d3d3b1aa64cdab
Victor Barrios Jimenez <vhbj149601@gmail.com>
2017-02-12 21:17:30 -0600

    updating views

--- src/app/shared/contact/contact.component.ts
+++ src/app/shared/contact/contact.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Producto } from '../../products';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-
+  powers = ['Really Smart', 'Super Flexible','Super Hot', 'Weather Changer'];
+  model = new Producto(1,"222k",3,"shirt",123,"clothing",.03,"lalalala","http:","Nike",[]);
+  submitted  = false;
+  onSubmit() { this.submitted = true; }
   constructor() { }
-
   ngOnInit() {
   }
 }
