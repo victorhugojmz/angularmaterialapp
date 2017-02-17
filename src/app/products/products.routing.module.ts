b2c436bd6e2099921c264b7bc9355f5bc5a367c6
Victor Barrios Jimenez <vhbj149601@gmail.com>
2017-02-16 18:42:48 -0600

    updating routes

--- src/app/products/products.routing.module.ts
+++ src/app/products/products.routing.module.ts
@@ -3,14 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 /* Components */
 import { ProductsComponent } from './products/products.component';
 import { DetailsComponent }  from './details/details.component';
-import { ContactComponent } from '../shared/contact/contact.component';
 import { AboutComponent } from '../shared/about.component';
-import { ComplexFormComponent } from '../shared/complex-form/complex-form.component';
+import { ContactComponent } from '../shared/contact/contact.component';
 
 const productRoutes: Routes = [
   { path: 'productos', component:  ProductsComponent},
   { path: 'productos/:departamento/:id',component: DetailsComponent},
-  { path: 'contacto', component:  ComplexFormComponent}, 
+  { path: 'contacto', component: ContactComponent }, 
   { path: 'about', component: AboutComponent}
 ];
 @NgModule({
