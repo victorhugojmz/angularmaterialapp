38656b775e5d3cdfba38aba2c730b16c44efc7a8
Victor Barrios Jimenez <vhbj149601@gmail.com>
2017-02-18 15:51:50 -0600

    updating the model form

--- src/app/products/product.class.ts
+++ src/app/products/product.class.ts
@@ -12,7 +12,7 @@ export class Producto {
     imagenes: Imagen[];
     marca?: string;
     specs?: Spec[ ];
-    constructor(public _id ?: number, public _sku?:  string, public _stock?: number, public _nombre?: string, public _precio?: number, public _departamento?: string, public _descuento?: number, public _detalles?: string,public _imagen?: string, public _marca?:  string, public _imagenes?: Imagen[], public _specs?: Spec[]){ 
+    constructor(public _id ?: number, public _sku?:  string, public _stock?: number, public _nombre?: string, public _precio?: number, public _departamento?: string, public _descuento?: number, public _descripcion?: string,public _imagen?: string, public _marca?:  string, public _imagenes?: Imagen[], public _specs?: Spec[]){ 
         this.id = _id;
         this.sku = _sku;
         this.stock = _stock;
@@ -20,7 +20,7 @@ export class Producto {
         this.precio = _precio;
         this.departamento = _departamento;
         this.descuento = _descuento;
-        this.detalles = _detalles;
+        this.descripcion = _descripcion;
         this.imagen = _imagen;
         this.imagenes = _imagenes;
         this.marca = _marca;
