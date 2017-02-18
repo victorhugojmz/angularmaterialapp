dc21fc1d05cf5a7f66bc298e3955adbb1f25fd71
Victor Barrios Jimenez <vhbj149601@gmail.com>
2017-02-18 14:21:47 -0600

    uploading images

--- src/app/shared/about.component.ts
+++ src/app/shared/about.component.ts
@@ -3,6 +3,9 @@ import {Component, OnInit } from '@angular/core';
   template: `
    <div class="container"> 
       <h1>About</h1>
+      <div class="col-md-6-col-xs-12 col-sm-6 col-lg-6">
+        <img src="" class="img img-resposive" alt="">
+      </div>
    </div>
   `
 })
