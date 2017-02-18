0eaf9185281eb020c862bafcd9b51b4a51ffb283
Victor Barrios Jimenez <vhbj149601@gmail.com>
2017-02-18 00:21:22 -0600

    updating class and new roduct component logic

--- src/app/products/departamento.ts
+++ src/app/products/departamento.ts
@@ -2,11 +2,9 @@ export class Departamento {
     public nombre: string;
     public descripcion ?: string;
     public hero?: string;
-    public imagenes?: Array<string>;
-    constructor(_nombre: string, _descripcion: string, _hero: string,_imagenes: string[]){
+    constructor(_nombre: string, _descripcion: string, _hero: string){
         this.nombre = _nombre;
         this.descripcion = _descripcion;
         this.hero = _hero;
-        this.imagenes =  _imagenes;
     }  
 }
