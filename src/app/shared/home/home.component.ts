edb41ba6d01f028886b0097e54811bf444fc124b
Victor Barrios Jimenez <vhbj149601@gmail.com>
2017-02-20 12:04:23 -0600

    updating templates

--- src/app/shared/home/home.component.ts
+++ src/app/shared/home/home.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { ProductsService } from '../../products/products.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-  data;
-  constructor(private ps : ProductsService) 
-  { }
-  ngOnInit() {
-    this.ps.load().subscribe(data  =>  console.log(data));
+export class HomeComponent  {
+  constructor() { }
+  public seleccionar(): void {
+    console.log("padre");
+  }
+  public eliminar($event): void {
+    $event.stopPropagation();
+    console.log("hijo");
   }
 }
