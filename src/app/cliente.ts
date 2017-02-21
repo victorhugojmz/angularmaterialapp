dca5029985db36bb8d5f7be31410acf30b88fd01
Victor Barrios Jimenez <vhbj149601@gmail.com>
2017-02-21 13:28:58 -0600

    setting up the constructor

--- src/app/cliente.ts
+++ src/app/cliente.ts
@@ -1,7 +1,14 @@
 export class Cliente {
-    id:  number;
-    nombre: string;
-    email: string;
-    direccion: string;
-    telefono: string;
+    public id:  number;
+    public nombre: string;
+    public email: string;
+    public direccion: string;
+    public telefono: string;
+    constructor(id: number, nombre: string , email: string, direccion: string, telefono: string){ 
+        this.id = id;
+        this.nombre = nombre;
+        this.email = email;
+        this.direccion = direccion;
+        this.telefono = telefono;
+    }
 }
