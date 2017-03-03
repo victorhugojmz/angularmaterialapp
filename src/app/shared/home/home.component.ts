b737b21ca825114666c5d55111624fd1d6c18e60
Victor Barrios Jimenez <vhbj149601@gmail.com>
2017-03-02 22:43:53 -0600

    updaitng components

--- src/app/shared/home/home.component.ts
+++ src/app/shared/home/home.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductsService, Departamento, departamentosQuerySet} from '../../products';
-import { } from '';
+import { Router } from '@angular/router';
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
   public departamentos: Departamento[];
-  constructor(private _productsService: ProductsService) { }
-   ngOnInit(): void {
+  constructor(private _productsService: ProductsService, private router : Router) { }
+  ngOnInit(): void {
       this.departamentos = departamentosQuerySet;
-  }                                                                                                    
+  }                                             
+  public OnSelectedDepartment(departamento: Departamento) {
+    this.router.navigate(['/departamentos'+ '/' + departamento.nombre]);
+  }                                                      
 }
