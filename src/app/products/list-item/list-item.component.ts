05c89f07738ccc299eb753be576b8b717be61b22
Victor Barrios Jimenez <vhbj149601@gmail.com>
2017-03-09 18:35:25 -0600

    updatinng component

--- src/app/products/list-item/list-item.component.ts
+++ src/app/products/list-item/list-item.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { Producto }  from '../../products';
 @Component({
   selector: 'app-list-item',
   templateUrl: './list-item.component.html',
   styleUrls: ['./list-item.component.css']
 })
 export class ListItemComponent implements OnInit {
-
+  @Input('product-data') public producto: Producto;
   constructor() { }
-
   ngOnInit() {
   }
-
 }
