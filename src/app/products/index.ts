02470938e0cab5992ec0c4d6cfc1ed00af47646d
Victor Barrios Jimenez <vhbj149601@gmail.com>
2017-04-10 21:40:53 -0500

    fixing bug that prevented compilation

--- src/app/products/index.ts
+++ src/app/products/index.ts
@@ -1,7 +1,6 @@
 export * from './product.class';
 export * from './products.service';
 export * from './departamento';
-export * from './departamentos';
 export * from './animations';
 export * from './forms.service';
 export * from './contacto.class'; 
