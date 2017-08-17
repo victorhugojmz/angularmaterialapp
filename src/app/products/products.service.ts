e1691294a54acc9e37c01515430ab2b926232abd
Victor Barrios Jimenez <vhbj149601@gmail.com>
2017-08-17 17:46:25 -0500

    updating service

--- src/app/products/products.service.ts
+++ src/app/products/products.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Rx';
 import { Http , Headers, Response  } from '@angular/http';
 import { Producto , Departamento } from '../products';
+import { getHeaders } from './headers';
 import 'rxjs/add/operator/map';
 @Injectable()
 export class ProductsService { 
- private url = "https://productos-e9bd9.firebaseio.com/";
+ private url = "http://localhost:8080/";
  constructor(private _http:  Http){ }
   public getProducts(departamento: string): Observable<Producto[]> {
-   return  this._http.get(this.url + departamento + '.json')
+   return  this._http.get(this.url + departamento,getHeaders())
                      .map(response =>  response.json()); 
   }
   public getProductPerRoute(departamento: string, id: any) : Observable<Producto> { 
@@ -16,7 +17,7 @@ export class ProductsService {
                .map((producto: Producto[]) => producto.find(producto => producto.id === id));
   }
   public getDepartaments( ): Observable<Departamento[]>{
-      return this._http.get(this.url + 'departamentos.json')
+      return this._http.get(this.url + 'departamentos',getHeaders())
                        .map((response: Response)=>response.json());
   }
   public getDepartment(nombreDepartamento: string): Observable<any> {
